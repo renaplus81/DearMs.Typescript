9f98b990b7c7c6935a92852c1732556a781b2e5b
Rena Takeuchi <takerena0322@icloud.com>
2026-06-19 06:55:42 +0000

    its4pmrightnow

---
+++ assessment/battlesys.ts
@@ -0,0 +1,71 @@
+// ---------------------------------------------------
+// 必須: Character クラスを完成させる
+// ---------------------------------------------------
+class Character {
+  name: string;
+  hp: number;
+  attackPower: number;
+ 
+  // コンストラクタ（ここはお手本として埋めてあります）
+  constructor(name: string, hp: number, attackPower: number) {
+    this.name = name;
+    this.hp = hp;
+    this.attackPower = attackPower;
+
+  }
+ 
+  // 相手を攻撃するメソッド
+
+  ///void: 戻り値を無視する
+  attack(target: Character): void {
+    // TODO: target の hp を this.attackPower の分だけ減らす
+    // TODO: 攻撃の結果を console.log で表示する
+    //       例) 「勇者 の攻撃！ スライム に 20 ダメージ（残りHP: 30）」
+
+    ///攻撃ダメージをhpからマイナス
+    ///最初はinterfaceを使ってたけど、interfaceはデータのかたを定義するもので、
+    ///functionは実際の処理(動的なアクション)を実行するものなので
+    ///変えた
+    ///thisつけるかつけないかわからなかったけど消したらエラーも消えた
+
+    ///残りダメージの表示方法むずい
+    function attackDmg(hp: number, attackPower: number, name: string){
+        //もしhpの方が攻撃より数字が大きい時(>=だと同じ時0になっちゃう)
+        if(hp > attackPower){
+            hp -= attackPower; 
+            //残りダメージの表示
+            //ここのnumberは型なので変数名ではあリャエセn
+            let remHp: number = hp -= attackPower;
+        console.log(`${name}が${attackPower}ダメージを与えた！(残り${remHp}HP。)`);
+        } else {
+        console.log(`${name}を倒した！`);
+    }
+
+
+
+  }
+}
+}
+ 
+ 
+// ---------------------------------------------------
+// 動作確認（必須ライン）
+// ---------------------------------------------------
+// TODO: 2体つくって attack を呼んでみましょう
+//
+ 
+ 
+// ---------------------------------------------------
+// コア: Warrior を継承でつくる
+// ---------------------------------------------------
+// TODO: Character を継承した Warrior クラスをつくる（extends）
+// TODO: attack をオーバーライドして、Character とは違う攻撃にする
+//       例) ダメージを2倍にする、専用のログを出す など
+  
+// ---------------------------------------------------
+// 発展（できた人向け）
+// ---------------------------------------------------
+// TODO: HP が 0 未満にならないように、ゲッター / セッターで管理する
+// TODO: 別の職業（Mage など）を追加する
+// TODO: Character[] に複数体を入れて、forEach などでまとめて攻撃させる
+// TODO: どちらかの hp が 0 以下になったらバトル終了、という流れをつくる
--- src/hello.ts
+++ src/hello.ts
@@ -1,28 +1,28 @@
-function calculateTotal(price: number, quantity: number): number{
-    return price * quantity;
-}
+// function calculateTotal(price: number, quantity: number): number{
+//     return price * quantity;
+// }
 
-const result: number = calculateTotal(100, 5);
+// const result: number = calculateTotal(100, 5);
 
-function printNames(names: string[]):void{
-    names.forEach(name => console.log(name));
-}
+// function printNames(names: string[]):void{
+//     names.forEach(name => console.log(name));
+// }
 
-printNames(["田中", "佐藤", "鈴木"]);
+// printNames(["田中", "佐藤", "鈴木"]);
 
-//interfaceで変数の型をまとめて指定できる(?)
+// //interfaceで変数の型をまとめて指定できる(?)
 
-const mixed: (string | number)[] = ["hello", 42, "world", 100];
+// const mixed: (string | number)[] = ["hello", 42, "world", 100];
 
-interface User {
-    name: string;
-    age: number;
-}
+// interface User {
+//     name: string;
+//     age: number;
+// }
 
-const users: User[] = [
-    {name: "太郎", age: 25 },
-    {name: "花子", age: 28 }
-];
+// const users: User[] = [
+//     {name: "太郎", age: 25 },
+//     {name: "花子", age: 28 }
+// ];
 
 //リテラル型：プリミティブ型の特定の値だけを代入可能にする型を表現できる
 // const age = 18;
@@ -40,12 +40,12 @@ const users: User[] = [
 // let id: number | string;　みたいな感じです。
 
 //エイリアス (type, Roleという名前の型を作っている)
-type Role = number | string; 
-type EventType = 'click' | 'hover' | 'keydown';
-//ユニオン型とリテラル型
+// type Role = number | string; 
+// type EventType = 'click' | 'hover' | 'keydown';
+// //ユニオン型とリテラル型
 
-let firstRole: Role;
-let lastRole: Role;
+// let firstRole: Role;
+// let lastRole: Role;
 
 //エイリアスのユニオン型っていうのもある
 
@@ -76,18 +76,228 @@ let lastRole: Role;
 
 //オプショナルプロパティ、なくてもあってもいい奴には？をつける。
 
-type Drink = | 'コーンポタージュ' |'ジャスミンティー'|'ミルクティー';
-type Price = 3|4|5;
-const items:{
-    name: Drink;
-    price: Price;
-}[]=[
-    {name:'コーンポタージュ', price: 3},
-    {name:'ジャスミンティー', price: 5},
-    {name:'ミルクティー', price: 4}
-];
+// type Drink = | 'コーンポタージュ' |'ジャスミンティー'|'ミルクティー';
+// type Price = 3|4|5;
+// const items:{
+//     name: Drink;
+//     price: Price;
+// }[]=[
+//     {name:'コーンポタージュ', price: 3},
+//     {name:'ジャスミンティー', price: 5},
+//     {name:'ミルクティー', price: 4}
+// ];
 //→→他の製品や価格の組み合わせはエラーになる
 //リテラル型＝値「そのもの」を型にすること。文字が型になる。
 //ユニオン型＝複数の型を｜でまとめること。
 //型エイリアス＝型に名前をつけること。
 //型を定義することで、何回もパイプを使わずに変数の型宣言に使うことができる（変数の再利用）
+
+
+//読み取り専用プロパティ
+//読み取り専用プロパティは値の変更は許可されない
+
+//Array型
+//↓は、number | stringという推測をしてくれる
+// const nums = [1,2,3,4,5, 'six'];
+
+//tuple型、javascriptには存在しないデータを表現できる型。
+// type RGB = [red: number, green: number, blue: number];
+//↑ラベル付きtuple🌷
+
+//インターセクション型
+//ユニオン型のように複数の型を組み合わせて新しい型を作成するため
+//何が違うかというと、｜ではなく&であるということ。
+// type Engine = {
+//     engineType: string;
+//     volume: number;
+// };
+
+// type Wheels = {
+//     wheelCount: number;
+// };
+
+// type Car = Engine & Wheels;
+
+// const myCar: Car = {
+//     engineType = 'V8',
+//     volume = 3000,
+//     wheelCount = 4,
+// };
+
+
+//any型（ほぼ使わないべき）
+//何でも入ってしまう
+// let value1: any = 1;
+// value1 = 'noTypeCheck';
+// value1.nocheck(); //実行してからじゃないとエラーがわからない。
+// let value2 = [1,2,3];
+// let value3 = value1 + value2;
+//型チェックがされないためエラーになる（？）
+
+//unknown型
+//anyと同じく、どんな型の値でも代入できる
+//どんな型が来るかわからないよ、と教えてくれる
+//別のデータをデータを取る時に、型がない場合もあるかもしれないという（？）
+// let value1: unknown = 1;
+// let value2: number = value1;
+
+
+// let value3: unknown = 10;
+// if(typeof value3 === 'number'){
+//     console.log(value3 + 1);
+// }
+
+
+//関数
+
+// //関数型↓↓↓↓
+// //:以降は変数の型。
+// let myFunction: (arg1:number, arg2:number)=> boolean;
+
+// myFunction = function(arg1, arg2){
+//     return arg1 > arg2;
+// };
+
+// console.log(myFunction(2,1));
+
+// //アロー関数のとき
+// myFunction = (arg1, arg2) => {
+//     return arg1 > arg2;
+// };
+// console.log(myFunction(2,1));
+
+
+// //これが関数型らしい
+// const addNumbers: (a: number, b: number) => number = (a, b) => a + b;
+// //const addNumbers　→ 定数
+// //=> numberまでがアロー関数の型定義
+// //=> numberに数値型を返す
+// //addにはnumberを２つ受け取ってnumberを返す関数しか入れられない
+// const addNumbers2: (a: number, b: number) => string = (a, b) => `${a} + ${b}`;
+// //戻り値がstringとなったら、文字列にしなくてはいけない(`${a} + ${b}`)
+// console.log(addNumbers(10,5));
+
+
+//関数型とエイリアス
+// type Calc = (a: number, b: number) => number;
+// const plusCalc: Calc = (a, b) => a + b;
+// const minusCalc: Calc = (a, b) => a - b;
+// const timesCalc: Calc = (a, b) => a * b;
+// const devideCalc: Calc = (a, b) => a / b;
+// console.log(devideCalc(10,5));
+
+
+// //インデックスシグネチャー　index signature
+// interface FruitStock {
+//     [i: string]: number;
+// }
+// //iは何個キーが追加されるかわからない（iではなくでもなんのアルファベットでもいい）
+// //文字列のキーに対してnumberが入る
+
+// const fruit: FruitStock = {};
+// fruit.apple = 3;
+// fruit.orange = 5;
+// fruit.banana = 'many'; //数字ではないのでエラー
+
+
+
+
+//クラス:めちゃくちゃ出てきます
+//クラスからできたものはインスタンスという
+// class Person {
+//     name: string;
+//     age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+// //インスタンス
+// const tom = new Person('Tom', 30);
+
+
+// //オプショナルプロパティと読み取り専用プロパティ
+// class Person{
+//     readonly name: string;
+//     age: number;
+//     hobbies?: string[];
+
+//     constructor(name: string, age: number){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet(greeting: string): void {
+//         console.log(`Hello ${greeting}.`);
+//     }
+// }
+
+// let tom: Person;
+// tom = new Person('Tom', 30);
+// tom.hobbies = ['running'];
+// console.log(tom.hobbies);
+// tom.greet(tom.name);
+// //まじでわかんない
+
+
+
+//クラスの継承:extends
+
+
+//アクセサ(setter, getter)
+
+// class Circle {
+//     private _radius: number;
+
+//     constructor(radius: number){
+//         this._radius = radius;
+//     }
+// //ゲッター
+//     get radius(): number {
+//         console.log('半径を取得');
+//         return this._radius;
+//     }
+
+// //セッター
+//     set radius(value: number){
+//         if(value <= 0){
+//             throw new Error('不正な値です');
+//         }
+//         console.log('半径を設定');
+//         this._radius = value; //これ何？
+//     }
+// }
+
+
+
+// //staticプロパティ
+// class Circle {
+//     static PI: number = 3.14;
+//     radius: number;
+    
+//     constructor(radius: number){
+//         this.radius = radius;
+//     }
+
+//     getArea():number{
+//         return Circle.PI * this.radius * this.radius;
+//     }
+// }
+// console.log(Circle.PT);
+
+// const circleA = new Circle(5);
+// console.log(circleA.getArea());
+
+// const circleB = new Circle(10);
+// console.log(circleB.getArea());
+
+
+// //抽象クラスと抽象メソッド
+// abstract class Animal {
+//     abstract makeSound(): void;
+
+//     move() : void{ //テキストには()なかったので注意
+//         console.log('The animal is moving.');
+//     }
+// }
